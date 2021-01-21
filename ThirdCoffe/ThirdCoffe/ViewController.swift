9502b8123d62e41f5c4e4379b46b14683a99e5fb
anh.tranle@ncc.asia <anh.tranle@ncc.asia>
2021-01-21 15:19:44 +0700

    There UI component of Project

---
+++ ThirdCoffe/ThirdCoffe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdCoffe
+//
+//  Created by le  anh on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
